c312a1f205ec0a038ad2358b492a216cb0cafb8e
Saha <404110@cognizant.com>
2019-01-31 16:17:59 +0530

    revert schema

--- server/api/v1/notes/notes.entity.js
+++ server/api/v1/notes/notes.entity.js
@@ -3,30 +3,30 @@ const Schema = mongoose.Schema;
 
 const notesSchema = new Schema({
   id: {
-    type: String
-    //unique: true,
-    //require: true
+    type: String,
+    unique: true,
+    require: true
   },
   title: {
-    type: String
-    //require: true
+    type: String,
+    require: true
   },
   text: {
-    type: String
-    //require: true
+    type: String,
+    require: true
   },
   state: {
-    type: String
-    //require: true,
-    //enum: ['started', 'not-started', 'completed']
+    type: String,
+    require: true,
+    enum: ['started', 'not-started', 'completed']
   },
   userId: {
-    type: String
-    //require: true
+    type: String,
+    require: true
   },
   createdOn: {
-    type: Date
-    //require: true
+    type: Date,
+    require: true
   },
   modifiedOn: {
     type: Date
